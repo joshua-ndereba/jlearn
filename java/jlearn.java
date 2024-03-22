c6749ef6c1e7742526ff452f47dda88f1839d950
joshua-ndereba <110087629+joshua-ndereba@users.noreply.github.com>
2024-03-22 17:59:03 +0300

    Add files via upload

---
+++ java/jlearn.java
@@ -0,0 +1,31 @@
+
+public class jlearn{
+
+    static void cars(){
+        String[][] day = {{"monday", "tuesday","wednesday"}, {"thursday","friday","saturday"}};
+        
+        
+        for(int i=0; i<=day.length; i++ ){
+            for (int j=0; j<=day.length; j++){
+                System.out.println(day[i][j]);
+            }
+        
+        }
+    }
+    public static void main(String[] args){
+        
+        cars();
+    }
+
+    
+}
+
+//length(), sqrt(), max(), min(), print(), sum(),
+
+
+   
+
+
+
+
+
